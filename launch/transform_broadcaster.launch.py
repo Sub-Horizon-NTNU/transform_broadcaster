34e459f165fe57d25986734086d5c2b04301bd6b
Liam <liam.folsviks@gmail.com>
2026-04-17 15:12:03 +0200

    Added launch with parameters

---
+++ launch/transform_broadcaster.launch.py
@@ -0,0 +1,34 @@
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    #Camera used Z UP, X FWD
+    camera_offset_x_arg = DeclareLaunchArgument('camera_offset_x',description='camera coordinates (left) relative to USV NED')
+    camera_offset_y_arg = DeclareLaunchArgument('camera_offset_y',description='camera coordinates (left) relative to USV NED')
+    camera_offset_z_arg = DeclareLaunchArgument('camera_offset_z',description='camera coordinates (left) relative to USV NED')
+
+    
+    model_arg = DeclareLaunchArgument('model',default_value='best.onnx',description='Select which model to use. e.g: best.onnx')
+    transform_broadcaster_node = Node(
+                            package='transform_broadcaster',
+                            executable='transform_broadcaster',
+                            name='transform_broadcaster',
+                            output='screen',
+                            parameters= [
+                                {"camera_offset_x":LaunchConfiguration("camera_offset_x")},
+                                {"camera_offset_y":LaunchConfiguration("camera_offset_y")},
+                                {"camera_offset_z":LaunchConfiguration("camera_offset_z")}
+                            ]
+                        )
+
+    return LaunchDescription([
+        camera_offset_x_arg,
+        camera_offset_y_arg,
+        camera_offset_z_arg,
+        model_arg,
+        transform_broadcaster_node
+    ])
